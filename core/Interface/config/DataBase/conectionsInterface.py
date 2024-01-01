177bf94f87dc8ad44b053e082d34a133626dadf0
jarvishuice <jarvis.realg@gmal.com>
2024-01-01 13:42:12 -0400

    ajustando la interface

--- core/Interface/config/DataBase/conectionsInterface.py
+++ core/Interface/config/DataBase/conectionsInterface.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
 
-class ConectionDbInterface(ABC):
-    ERROR="\033[0;31m"
-    NOTE="\033[0;33m"
-    DATABASE_ERROR=None
-    INTEGRIDAD_ERROR=None
-    INTERFACE_ERROR=None
-    OPERATIONAL_ERROR=None
+
+class ConectionDbInterface (ABC):
+    ERROR = "\033[0;31m"
+    NOTE = "\033[0;33m"
+    DATABASE_ERROR = None
+    INTEGRIDAD_ERROR = None
+    INTERFACE_ERROR = None
+    OPERATIONAL_ERROR = None
 
     @abstractmethod
     def connect(self):
@@ -20,22 +21,21 @@ class ConectionDbInterface(ABC):
 
     def readAll(self) -> dict:
         pass
-    def readOneById(self, id ) -> dict:
+    def readOneById(self, id) -> dict:
         pass
 
-    def search(self,param)->dict:
+    def search(self, param) -> dict:
         pass
     # read one register by param
-    def readOneByParam(self,param) ->dict:
+    def readOneByParam(self, param) -> dict:
         pass
-    #read all by status
-    def readOneByStatus(self,status)-> dict:
+
+    def readOneByStatus(self, status) -> dict:
         pass
-    def readAllTwoParm(self,param,param2)-> dict:
+    def readAllTwoParm(self, param, param2) -> dict:
         pass
-    #lectura de todos los detalles de la entidad vinculada en distintas tablas
-    def readDetailEntity(self,id)-> dict:
+    def readDetailEntity(self, id) -> dict:
         pass
-    #actualizar la cantidad de un campo por un id
-    def  updateCantidadById(self,id,catidad,sede)-> dict :
+    def updateCantidadById(self, id, catidad, sede) -> dict:
+
         pass
