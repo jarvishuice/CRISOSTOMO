a2108df6a4bfc2dfaafbb9d71ea636dd37e7fbb9
jarvishuice <jarvis.realg@gmal.com>
2024-01-02 14:39:26 -0400

    Subiendo documentacion

--- config/Logs/LogsActivity.py
+++ config/Logs/LogsActivity.py
@@ -19,6 +19,13 @@ class Logs :
             print(error)
     @staticmethod
     def Warnings(mensaje:str):
+        '''
+        Este código es un método estático que se utiliza para registrar advertencias en un archivo de registro. Toma un mensaje como entrada y lo agrega al archivo de registro "systemLog.log" junto con la marca de tiempo actual.
+
+        Ejemplo de uso:
+        LogsActivity.Warnings("Este es un mensaje de advertencia")
+
+        '''
         try:
             with open(f'{PathMunay().getLogs()}systemLog.log', 'a') as archivo :
                 archivo.write(f' \033[0;36m[{datetime.datetime.now()}]:\033[0;33m {mensaje}\n')
